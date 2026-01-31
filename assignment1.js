d1d51a5917cce5b47b080d296644a4af92a46545
Deepandisney <deepannashok@gmail.com>
2026-01-31 22:39:32 +0530

    Add files via upload
    Day 1 assignment

---
+++ assignment1.js
@@ -0,0 +1,16 @@
+let username ="Deepan"
+console.log("my name is"+username +(typeof(username)))
+let organization ="HCLTech"
+console.log(organization)
+let contactnumber=7708772852
+contactnumber="12345"
+console.log("my contactnumber is" +contactnumber +typeof(contactnumber))
+
+let a =((8>3)&&(1>3))
+console.log(a)
+
+let b=((5>3)||(3<5))
+console.log(b)
+
+let usesplaywright
+console.log("playwrigt is" +usesplaywright +typeof(usesplaywright))
